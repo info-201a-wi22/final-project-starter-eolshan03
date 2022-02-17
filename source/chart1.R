5d3be39536eebf20e7bf6102c57e84c6ae3e46e4
Samira Shirazy <samira03@uw.edu>
2022-02-17 02:09:04 -0800

    Wrangle data frame

--- source/chart1.R
+++ source/chart1.R
@@ -2,6 +2,7 @@
 library("dplyr")
 library("tidyr")
 library("ggplot2")
+library("stringr")
 
 # Set working directory 
 setwd("C:/Users/Samira Shirazy/Desktop/final-project-starter-eolshan03")
@@ -9,7 +10,18 @@ getwd()
 
 # Load homelessness data
 filename <- "data/homelessness-2007-2016.csv"
-homeless <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
+homeless_data <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
 
-# Create Map 
-state_shape <- map_data("state")
+View(homeless_data)
+
+# explain what this means pls
+ah <- homeless_data %>%
+  select(Year, State, Count, Measures) %>%
+  mutate(yr = as.numeric(substring(Year, 5, 8))) %>%
+  group_by(yr) %>%
+  mutate(boi = as.numeric(gsub("[,]","",Count))) %>%
+  summarize(yearly_tot = sum(boi, na.rm = TRUE))
+View(ah)
+
+# Create Area Chart
+
