18c352f94093b28a08935d6092489b45405677f4
Samira Shirazy <samira03@uw.edu>
2022-02-17 03:37:54 -0800

    Add comments

--- source/chart1.R
+++ source/chart1.R
@@ -14,18 +14,19 @@ homeless_data <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
 
 View(homeless_data)
 
-# explain what this means pls
+# This chart will find the total sum of all the homelessness categories in each
+# year. Then, 
 homelessness_by_year <- homeless_data %>%
   select(Year, State, Count, Measures) %>%
-  mutate(yr = as.numeric(substring(Year, 5, 8))) %>%
-  group_by(yr) %>%
-  mutate(boi = as.numeric(gsub("[,]","",Count))) %>%
-  summarize(yearly_tot = sum(boi, na.rm = TRUE))
-View(ah)
+  mutate(year = as.numeric(substring(Year, 5, 8))) %>%
+  group_by(year) %>%
+  mutate(population_sum = as.numeric(gsub("[,]","", Count))) %>%
+  summarize(yearly_total = sum(population_sum, na.rm = TRUE))
+View(homelessness_by_year)
 
 
-# draft: Create Area Chart
-ggplot(homelessness_by_year, aes(x = yr, y = yearly_tot)) +
+# Creating the chart
+ggplot(homelessness_by_year, aes(x = year, y = yearly_total)) +
   ylim(0, 3200000) +
   scale_y_continuous(labels = scales::comma) +
   geom_area(
@@ -40,9 +41,8 @@ ggplot(homelessness_by_year, aes(x = yr, y = yearly_tot)) +
   ) +
   labs (
     title = "Homeless Population in U.S. by year",
-    subtitle = "Sum of homelessness populations from 2007 to 2016",
+    subtitle = "Sum of the homelessness populations from 2007 to 2016",
     x = "Year",
     y = "Population Sum",
     color = "Urbanity"
   )
-  
