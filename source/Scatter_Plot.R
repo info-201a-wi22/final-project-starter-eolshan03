497449c3024011e0299e148637d3ff8c87ca25eb
Samira Shirazy <samira03@uw.edu>
2022-02-18 01:57:46 -0800

    Fix warnings

--- source/Scatter_Plot.R
+++ source/Scatter_Plot.R
@@ -4,17 +4,15 @@ library("ggplot2")
 library("tidyr")
 library("stringr")
 
-setwd("~/Documents/Group Project/final-project-starter-eolshan03")
-homelessness <- read.csv("data/homelessness-2007-2016.csv", stringsAsFactors = FALSE)
+homelessness <- read.csv("../data/homelessness-2007-2016.csv", stringsAsFactors = FALSE)
 
 #Create data for NY and WA total homeless count from 2007-2016
 the_homeless <- homelessness %>%
-  group_by(Year,State) %>%
+  group_by(Year, State) %>%
   filter(State == "WA" || State == "NY") %>%
-  summarize(Count = sum(as.numeric(Count), na.rm = TRUE)) %>%
+  summarise(Count = sum(as.numeric(gsub("[,]","", Count), na.rm = TRUE))) %>%
   mutate(Year = as.numeric(substring(Year, 5,8)))
-  View(the_homeless)
-  
+
 #Creating the Scatter Plot
 scatter_plot <- ggplot(data = the_homeless) +
   geom_point(
@@ -32,4 +30,4 @@ scatter_plot <- ggplot(data = the_homeless) +
   y = "Total Homeless Count",
   color = "Location") +
   theme(plot.title = element_text(face ="bold", size=15)
-  )
+)
