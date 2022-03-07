3ad384fd88ac968a8c6cff2bdddd9b8b9bfd34b8
Emiy <97005763+eolshan03@users.noreply.github.com>
2022-03-07 12:50:35 -0800

    changes

--- data/bar_chart.R
+++ data/bar_chart.R
@@ -11,7 +11,6 @@ pit_homeless <- homeless %>%
   group_by(year) %>%
   mutate(total = sum(value, na.rm = T))
 
-
 total_area_type <- function(area_type) {
   pit_homeless %>%
     group_by(year) %>%
@@ -42,18 +41,19 @@ city <- total_area_type("Major Cities") %>%
 pit_homeless <- pit_homeless %>%
   group_by(year, total) %>%
   summarize()
-
-
+  
 join <- left_join(rural, suburban, by = "year")
 join2 <- left_join(urban, city, by = "year")
-homeless_by_area <- left_join(join, join2, by = "year")
+join3 <- left_join(join, join2, by = "year")
+homeless_by_area <- left_join(join3, pit_homeless, by = "year")
 
 homeless_pop <- homeless_by_area %>%
   select(year, total_rural, total_suburban, total_city, total_urban) %>%
-  gather(key = area, value = pop, -year)
+  gather(key = , value = pop, -year)
 
-final_bar_chart <- ggplot(homeless_pop) +
+bar_chart <- ggplot(homeless_pop) +
   geom_col(mapping = aes(year, pop, fill = area)) +
   ylab("Homeless Population") +
   scale_fill_discrete(labels = c("total city homeless", "total rural homeless", "total suburban homeless", "total urban homeless")) +
   labs(fill = "Community Type", title = "Homeless Pop over Time \n by Community Type")
+
